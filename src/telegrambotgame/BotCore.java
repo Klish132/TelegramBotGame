e43d11f1cc448d12251c94215e964cf7c1945d99
unknown <tuha99@mail.ru>
2018-10-21 18:52:51 +0500

    Added Tests

---
+++ src/telegrambotgame/BotCore.java
@@ -0,0 +1,129 @@
+package telegrambotgame;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.ArrayList;
+
+public class BotCore {
+
+	public String ongoing_game = "None";
+	public TttGrid ttt_grid = new TttGrid();
+	public TttPlayer ttt_player = new TttPlayer();
+	public TttAI ttt_AI = new TttAI();
+	public ArrayList<String> output_msg = new ArrayList<String>();
+	
+	public ArrayList<String> Analyze(String input_msg) {
+		
+		output_msg.clear();
+		
+		switch(this.ongoing_game) {
+		case "None":
+			
+			switch(input_msg) {
+			case "/ttt":
+				this.ongoing_game = "ttt";
+				output_msg.add("Please enter your side: \n x or o");
+				break;
+			} break;
+			case "/mb":
+				break;
+			case "/hangman":
+				break;
+			
+		case "ttt":
+			
+			switch(input_msg) {
+			case "x":
+				ttt_setSide("x", "o"); break;
+			case "o":
+				ttt_setSide("o", "x"); break;
+			case "/exit":
+				reset_bot();
+				output_msg.add("Stopping TickTackToe...");
+				break;
+			default:
+				ttt_doAction(input_msg);
+				if (ttt_checkGameOver() == true) {
+					reset_bot();
+				}
+				break;
+			}
+		}
+		
+		return output_msg;
+		}
+	
+	public void reset_bot( ) {
+		this.ongoing_game = "None";
+		this.ttt_grid = new TttGrid();
+		this.ttt_player = new TttPlayer();
+		this.ttt_AI = new TttAI();
+	}
+	
+	public String ttt_drawGrid(TttGrid grid) {
+		StringJoiner sj = new StringJoiner(System.lineSeparator());
+		for(char[] row : grid.grid_array) {
+			sj.add(Arrays.toString(row));
+		}
+		return sj.toString();
+	}
+	
+	public void ttt_setSide(String player_side, String ai_side) {
+		if (this.ttt_player.player_side == '_') {
+			this.ttt_player.setSide(player_side);
+			this.ttt_AI.setSide(ai_side);
+			output_msg.add("you are now playing as: " + player_side);
+			output_msg.add(ttt_drawGrid(this.ttt_grid));
+		}
+	}
+	public void ttt_doAction(String move) {
+		if (this.ttt_player.player_side != '_') {
+			Set<String> good_moves = new HashSet<String>(); {
+				for (int i=1; i<=3; i++) {
+					good_moves.add("A" + String.valueOf(i));
+					good_moves.add("B" + String.valueOf(i));
+					good_moves.add("C" + String.valueOf(i));
+				}
+			}
+			if (good_moves.contains(move)) {
+				if (this.ttt_grid.modifyGrid(move, this.ttt_player.player_side) == false) {
+					this.ttt_AI.doMove(this.ttt_grid);
+					output_msg.add(ttt_drawGrid(this.ttt_grid));
+				} else {
+					output_msg.add("This spot is taken!");
+				}
+			}
+		}
+		else {
+			output_msg.add("Something is wrong, please restart ttt...");
+		}
+	}
+	public boolean ttt_checkGameOver() {
+		
+		boolean result = true;
+		
+		switch (this.ttt_AI.checkGameOver(this.ttt_grid)) {
+		case "x":
+			output_msg.add("Winner: X");
+			break;
+		case "o":
+			output_msg.add("Winner: O");
+			break;
+		case "xo":
+			if (this.ttt_player.player_side == 'x') {
+				output_msg.add("Winner: X");
+			} else {
+				output_msg.add("Winner: O");
+			}
+			break;
+		case "draw":
+			output_msg.add("Game ended in a DRAW!");
+			break;
+		default:
+			result = false;
+			break;
+		}
+		return result;
+	}
+}
---
+++ test/telegrambotgame/BotCoreTest.java
@@ -0,0 +1,188 @@
+package telegrambotgame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+import org.junit.jupiter.api.Test;
+
+class BotCoreTest {
+	
+	BotCore testCore = new BotCore();
+	
+	public String drawGrid(char[][] char_arr) {
+		StringJoiner sj = new StringJoiner(System.lineSeparator());
+		for(char[] row : char_arr) {
+			sj.add(Arrays.toString(row));
+		}
+		return sj.toString();
+	}
+	
+	@Test
+	public void testChooseGame() {
+			
+			testCore.ongoing_game = "None";
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add("Please enter your side: \n x or o");
+			
+			assertEquals(result, testCore.Analyze("/ttt"));
+		}
+	
+	@Test
+	public void testExitTTT() {
+			
+			testCore.ongoing_game = "ttt";
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add("Stopping TickTackToe...");
+			
+			assertEquals(result, testCore.Analyze("/exit"));
+		}
+	
+	@Test
+	public void testChooseSideTTT() {
+			
+			char[][] empty_grid = new char[][]{
+				  { '_', '_', '_' },
+				  { '_', '_', '_' },
+				  { '_', '_', '_' }
+				};
+			
+			testCore.ongoing_game = "ttt";
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add("you are now playing as: x");
+			result.add(drawGrid(empty_grid));
+			
+			assertEquals(result, testCore.Analyze("x"));
+		}
+	
+	@Test
+	public void testVerticalTTT() {
+		
+		char[][] start_grid = new char[][]{
+			  { '_', 'x', '_' },
+			  { '_', '_', '_' },
+			  { '_', '_', '_' }
+			};
+		char[][] end_grid = new char[][]{
+			  { '_', 'x', '_' },
+			  { '_', 'x', '_' },
+			  { '_', 'o', '_' }
+			};
+		
+		testCore.ongoing_game = "ttt";
+		testCore.ttt_player.setSide("x");
+		testCore.ttt_AI.setSide("o");
+		testCore.ttt_grid.grid_array = start_grid;
+		
+		ArrayList<String> result = new ArrayList<String>();
+		result.add(drawGrid(end_grid));
+		
+		assertEquals(result, testCore.Analyze("B2"));
+	}
+	
+	@Test
+	public void testHorizontalTTT() {
+			
+			char[][] start_grid = new char[][]{
+				  { '_', '_', '_' },
+				  { 'x', '_', '_' },
+				  { '_', '_', '_' }
+				};
+			char[][] end_grid = new char[][]{
+				  { '_', '_', '_' },
+				  { 'x', 'x', 'o' },
+				  { '_', '_', '_' }
+				};
+			
+			testCore.ongoing_game = "ttt";
+			testCore.ttt_player.setSide("x");
+			testCore.ttt_AI.setSide("o");
+			testCore.ttt_grid.grid_array = start_grid;
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add(drawGrid(end_grid));
+			
+			assertEquals(result, testCore.Analyze("B2"));
+		}
+	
+	@Test
+	public void test1DiagonalTTT() {
+			
+			char[][] start_grid = new char[][]{
+				  { 'x', '_', '_' },
+				  { '_', '_', '_' },
+				  { '_', '_', '_' }
+				};
+			char[][] end_grid = new char[][]{
+				  { 'x', '_', '_' },
+				  { '_', 'x', '_' },
+				  { '_', '_', 'o' }
+				};
+			
+			testCore.ongoing_game = "ttt";
+			testCore.ttt_player.setSide("x");
+			testCore.ttt_AI.setSide("o");
+			testCore.ttt_grid.grid_array = start_grid;
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add(drawGrid(end_grid));
+			
+			assertEquals(result, testCore.Analyze("B2"));
+		}
+	
+	@Test
+	public void test2DiagonalTTT() {
+			
+			char[][] start_grid = new char[][]{
+				  { '_', '_', 'x' },
+				  { '_', '_', '_' },
+				  { '_', '_', '_' }
+				};
+			char[][] end_grid = new char[][]{
+				  { '_', '_', 'x' },
+				  { '_', 'x', '_' },
+				  { 'o', '_', '_' }
+				};
+			
+			testCore.ongoing_game = "ttt";
+			testCore.ttt_player.setSide("x");
+			testCore.ttt_AI.setSide("o");
+			testCore.ttt_grid.grid_array = start_grid;
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add(drawGrid(end_grid));
+			
+			assertEquals(result, testCore.Analyze("B2"));
+		}
+	
+	@Test
+	public void testBetweenTTT() {
+			
+			char[][] start_grid = new char[][]{
+				  { '_', '_', '_' },
+				  { 'x', '_', '_' },
+				  { '_', '_', '_' }
+				};
+			char[][] end_grid = new char[][]{
+				  { '_', '_', '_' },
+				  { 'x', 'o', 'x' },
+				  { '_', '_', '_' }
+				};
+			
+			testCore.ongoing_game = "ttt";
+			testCore.ttt_player.setSide("x");
+			testCore.ttt_AI.setSide("o");
+			testCore.ttt_grid.grid_array = start_grid;
+			
+			ArrayList<String> result = new ArrayList<String>();
+			result.add(drawGrid(end_grid));
+			
+			assertEquals(result, testCore.Analyze("C2"));
+		}
+	
+}
